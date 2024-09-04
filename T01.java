2367deb92c9cdcdf23c89b33f679b24be2bb1a14
MiaSibuea <miasibuea152@gmail.com>
2024-09-04 21:36:24 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,9 +1,7 @@
 // 12S24005 - Mia Nathania Sibuea
 // 12S24005 - Mia Nathania Sibuea
-
 import java.util.*;
 import java.lang.Math;
-
 public class Program {
     private static Scanner input = new Scanner(System.in);
 
