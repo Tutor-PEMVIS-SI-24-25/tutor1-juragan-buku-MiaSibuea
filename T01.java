b8e57cfb294e719b8569b92db9438837e13fb6c2
MiaSibuea <miasibuea152@gmail.com>
2024-09-04 21:26:57 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24005 - Mia Nathania Sibuea
+// 12S24005 - Mia Nathania Sibuea
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+
+        iSBN = input.nextLine();
+        System.out.println(iSBN);
+    }
+}
